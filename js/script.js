9dc28a345752c669507bf96d1fff1626f54a3bbf
marcocalligaris <marcocally@gmail.com>
2022-07-06 16:37:10 +0200

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -19,3 +19,39 @@
 
 // #Milestone 3
 // Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+
+//* Recupero dal DOM gli elemnti che mi servono
+
+let postsList = document.querySelector('.posts-list');
+const postCard = document.querySelector('.post');
+const postIcon = document.querySelector('.post-meta__icon');
+const profilePic = document.querySelector('.profile-pic');
+const authorName = document.querySelector('.post-meta__author');
+const postDate = document.querySelector('.post-meta__time');
+const postText = document.querySelector('.post__text');
+const postImage = document.querySelector('.post__image');
+const likeButton = document.querySelector('.js-like-button');
+const likesCounter = document.querySelector('.js-likes-counter');
+
+//* Creo l'array con gli oggetti 
+
+const postStructure = [
+    {
+        id: 1,
+        author: 'Phil Mangione',
+        profilePhoto: 'https://unsplash.it/300/300?image=15',
+        date: '07-06-2022',
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        image: 'https://unsplash.it/600/300?image=171',
+        likes: 80,
+    },
+    {
+        id: 1,
+        author: 'Phil Mangione',
+        profilePhoto: 'https://unsplash.it/300/300?image=15',
+        date: '07-06-2022',
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        image: 'https://unsplash.it/600/300?image=171',
+        likes: 80,
+    },
+]
