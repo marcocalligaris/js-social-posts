284eb1dd7b6302694c2c0870d78a3dff9354831f
marcocalligaris <marcocally@gmail.com>
2022-07-08 16:48:30 +0200

    Fine

--- js/script.js
+++ js/script.js
@@ -1,29 +1,17 @@
-// # Descrizione
-// Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+//* Funzioni
 
-// # Milestone 1
-// Creiamo il nostro array di oggetti che rappresentano ciascun post.
-// Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
-// - id del post, numero progressivo da 1 a n
-// - nome autore,
-// - foto autore,
-// - data in formato americano (mm-gg-yyyy),
-// - testo del post,
-// - immagine (non tutti i post devono avere una immagine),
-// - numero di likes.
-// *Non è necessario creare date casuali, inventatele*
-// *Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=3)*
-
-// #Milestone 2
-// Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
-
-// #Milestone 3
-// Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+const increaseProperty = (arr, key) => {
+  let number = 0;
+  for (item of arr) {
+    number += item.key++;
+  }
+}
 
 //* Recupero dal DOM gli elemnti che mi servono
 
 let postsList = document.querySelector('.posts-list');
-const likeButton = document.querySelector('.js-like-button');
+let likeLabel = document.querySelector('.like-button__label');
+const likeButtons = document.querySelectorAll('.js-like-button');
 const likesCounter = document.querySelector('.js-likes-counter');
 
 //* Creo l'array con gli oggetti 
@@ -36,7 +24,7 @@ const postStructure = [
         date: '07-06-2022',
         text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
         image: 'https://unsplash.it/600/300?image=171',
-        likes: 80,
+        likes: 0,
     },
     {
         id: 2,
@@ -45,7 +33,7 @@ const postStructure = [
         date: '10-05-2021',
         text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
         image: 'https://unsplash.it/600/300?image=80',
-        likes: 80,
+        likes: 5,
     },
 ]
 
@@ -54,14 +42,13 @@ console.table(postStructure)
 let postCards = '';
 for (let i = 0; i < postStructure.length; i++) {
     const label = postStructure[i];
-
     postCards += `
     <div class="post">
     <div class="post__header">
       <div class="post-meta">
-        <div class="post-meta__icon">
-          <img class="profile-pic" src="${label.profilePhoto}" alt="${label.authorName}" />
-        </div>
+      <div class="post-meta__icon">
+      <img class="profile-pic" src="${label.profilePhoto}" alt="${label.authorName}" />
+      </div>
         <div class="post-meta__data">
           <div class="post-meta__author">${label.authorName}</div>
           <div class="post-meta__time">${label.date}</div>
@@ -72,22 +59,35 @@ for (let i = 0; i < postStructure.length; i++) {
     ${label.text}
     </div>
     <div class="post__image">
-      <img src="${label.image}" alt="" />
+    <img src="${label.image}" alt="${label.id}" />
     </div>
     <div class="post__footer">
-      <div class="likes js-likes">
+    <div class="likes js-likes">
         <div class="likes__cta">
-          <a class="like-button js-like-button" href="#" data-postid="1">
-            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+          <a class="like-button js-like-button" href="#" data-postid="${label.id}">
+          <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
             <span class="like-button__label">Mi Piace</span>
           </a>
         </div>
-        <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">${label.likes}</b> persone</div>
+        <div class="likes__counter">Piace a <b id="like-counter-${label.id}" class="js-likes-counter">${label.likes}</b> persone</div>
       </div>
-    </div>
-  </div>
+      </div>
+      </div>
     `;
 }
 
 postsList.innerHTML = postCards;
+
+
+for(const button of likeButtons) {
+  button.addEventListener ('click', () => {
+    event.preventDefault();
+    button.classList.add('like-button--liked');
+
+    const postId = button.dataset.postid;
+    const likesCounter = document.getElementById(`like-counter-${postId}`);
+    let likes = parseInt(likesCounter.innerText);
+    likesCounter.innerText = ++likes;
+  })
+}
 
